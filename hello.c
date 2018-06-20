fce2238be22a1b57d5a3d5d6eeb0ddde2b862bc5
Anirudh Pal <pal5@purdue.edu>
2018-06-20 07:55:47 -0400

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,8 @@
+// This is a library that allows you to print on your screen.
+#include <stdio.h>
+
+// This is called the main of the code and is what runs first during execution.
+int main() {
+   // TODO: Place your code in the next line.
+   
+}
